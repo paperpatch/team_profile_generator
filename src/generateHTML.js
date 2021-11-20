464671eaf0973bd732ee1159d2c6a318dd5adac4
paperpatch <patchen21@gmail.com>
2021-11-19 21:29:44 -0500

    added links to email/github for html

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -9,7 +9,7 @@ function generateManagerHTML(manager) {
       <div class="card-body">
         <ul class="list-group list-group-flush">
           <li class="list-group-item">ID: ${manager.id}</li>
-          <li class="list-group-item">Email: ${manager.email}</li>
+          <li class="list-group-item">Email: <a href="mailto:${manager.email}">${manager.email}</li>
           <li class="list-group-item">Office Number: ${manager.officeNumber}</li>
         </ul>
       </div>
@@ -29,8 +29,8 @@ function generateEngineerHTML(engineer) {
       <div class="card-body">
         <ul class="list-group list-group-flush">
           <li class="list-group-item">ID: ${engineer.id}</li>
-          <li class="list-group-item">Email: ${engineer.email}</li>
-          <li class="list-group-item">Github: ${engineer.github}</li>
+          <li class="list-group-item">Email: <a href="mailto:${engineer.email}">${engineer.email}</li>
+          <li class="list-group-item">Github: <a href="https://github.com/${engineer.github}">${engineer.github}</a></li>
         </ul>
       </div>
     </div>
@@ -49,7 +49,7 @@ function generateInternHTML(intern) {
         <div class="card-body">
           <ul class="list-group list-group-flush">
             <li class="list-group-item">ID: ${intern.id}</li>
-            <li class="list-group-item">Email: ${intern.email}</li>
+            <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</li>
             <li class="list-group-item">School: ${intern.school}</li>
           </ul>
         </div>
@@ -63,7 +63,6 @@ function generateTeam(data) {
   pageData = [];
 
   for (let i =0; i< data.length; i++) {
-
     const employee = data[i];
     const role = employee.getRole();
     // console.log(role);
